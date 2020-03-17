7c7540bfed56c0e7d3ba5e52a60a345c11fd4dd5
Anni1123 <kumarianni1213@gmail.com>
2020-03-17 15:22:38 +0530

    ImageUploading

--- app/src/main/java/com/example/blogging/PostActivity.java
+++ app/src/main/java/com/example/blogging/PostActivity.java
@@ -1,14 +1,39 @@
 package com.example.blogging;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PostActivity extends AppCompatActivity {
 
+    private ImageView selectimage;
+    private static final int GALLERY_REQUEST=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        selectimage=(ImageView)findViewById(R.id.image);
+        selectimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent=new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent,GALLERY_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_REQUEST&&resultCode==RESULT_OK){
+            Uri imageuri=data.getData();
+            selectimage.setImageURI(imageuri);
+        }
     }
 }
